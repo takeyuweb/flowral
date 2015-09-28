285c9826ecb44bce2652821b2383f4f9819d3b13
yuka2py <yuka2py@gmail.com>
2015-09-28 11:55:26 +0900

    update

---
+++ src/flowral.js
@@ -0,0 +1,14 @@
+
+import FlowralContext_ from './FlowralContext';
+import FlowralAction_ from './FlowralAction';
+import FlowralStore_ from './FlowralStore';
+import FlowralDispatcher_ from './FlowralDispatcher';
+import * as utils_ from './utils';
+
+
+export let FlowralContext = FlowralContext_;
+export let FlowralAction = FlowralAction_;
+export let FlowralStore = FlowralStore_;
+export let FlowralDispatcher = FlowralDispatcher_;
+export let utils = utils_;
+
